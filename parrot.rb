5590530cac0e1aa12b6569512c51b7f1876974d2
Paul Holmes <paulholmes1993@gmail.com>
2019-05-11 20:00:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot_ruby(squawk)
+  puts squawk 
+  return  
+end 
+
+def parrot("relax"="Squawk!")
+  puts parrot
+  parrot
+end
